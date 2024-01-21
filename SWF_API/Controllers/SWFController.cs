c85582000ed527874c52f3ae8c6acbdf2674d866
ulisesmatysiak <ulisesmatysiak2@gmail.com>
2024-01-20 23:50:19 -0300

    campeonatos

--- SWF_API/Controllers/SWFController.cs
+++ SWF_API/Controllers/SWFController.cs
@@ -15,11 +15,45 @@ namespace SWF_API.Controllers
             _dbcontext = dbcontext;
         }
 
-        [HttpGet]
-        [Route("Listar")]
-        public IActionResult Listar()
+        [HttpPost]
+        [Route("InsertCampeonato")]
+        public ActionResult Insertar_Campeonatos([FromBody]Campeonato campeonato)
         {
-            
+            try
+            {
+                _dbcontext.Campeonatos.Add(campeonato);
+                _dbcontext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Campeonato con id: " + campeonato.Id + " descripcion: " + campeonato.Descripcion + " insert ok"});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+            }
+        }
+
+        [HttpPut]
+        [Route("EditCampeonato")]
+        public ActionResult Edit_Campeonatos([FromBody]Campeonato campeonato)
+        {
+            Campeonato edit = _dbcontext.Campeonatos.Find(campeonato.Id);
+            var old = edit.Descripcion;
+
+            if (edit == null)
+            {
+                return BadRequest("Campeonato no encontrado");
+            }
+
+            try
+            {
+                edit.Descripcion = campeonato.Descripcion;
+                _dbcontext.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Campeonato con id: " + campeonato.Id + " descripcion: " + old + " editado ok a: " + edit.Descripcion});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = ex.Message });
+            }
         }
     }
 }
