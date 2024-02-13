0a0aa3b01322a5790deb480d9261b1b8093f4c8f
ulisesmatysiak <ulisesmatysiak2@gmail.com>
2024-02-12 22:17:02 -0300

    kickoff bot

---
+++ SWF_BOT/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using SWF_BOT;
+using System.Text.Json;
+
+HttpClient httpClient = new HttpClient();
+
+var response = await httpClient.GetAsync("http://localhost:5076/api/SWF/Listar");
+
+if (response.IsSuccessStatusCode)
+{
+    var responseContent = await response.Content.ReadAsStringAsync();
+    var tweets = JsonConvert.DeserializeObject<List<Tweet>>(responseContent);
+
+    foreach (var tweet in tweets)
+    {
+        Console.WriteLine($"Nombre: {tweet.Nombre}, Camiseta: {tweet.Camiseta}, Imagen: {tweet.Imagen}, Campeonato: {tweet.Campeonato}, Fecha: {tweet.Fecha}");
+    }
+}
+else
+{
+    Console.WriteLine($"Error al obtener los tweets. Código de estado: {response.StatusCode}");
+}
---
+++ SWF_BOT/Tweet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWF_BOT
+{
+    public class Tweet
+    {
+        public string? Nombre { get; set; }
+        public int Camiseta { get; set; }
+        public string? Campeonato { get; set; }
+        public string? Imagen { get; set; }
+        public string? Fecha { get; set; }
+    }
+}
